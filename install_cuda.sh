fdef09f7ea187680a568bf79a4b3491caa9cdab0
Dang Huynh Lam Hai <22542993+HaiDang2001VN@users.noreply.github.com>
2018-11-16 00:50:36 +0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,8 +1,6 @@
-CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
-sudo dpkg -i ${CUDA_REPO_PKG}
-sudo apt-get update
-sudo apt-get -y install cuda
+CUDA_PKG="cuda-8-0_8.0.61-1_amd64.deb"
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_PKG}
+sudo apt-get -y install ${CUDA_PKG}
 
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
